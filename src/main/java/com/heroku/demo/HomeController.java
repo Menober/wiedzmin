413e453e635fbf28e19245a3677d8fb6f63c5544
Menober <krzysztof.miarczynski@gmail.comm>
2021-01-14 22:09:28 +0100

    Class optimalized for postgres

--- src/main/java/com/heroku/demo/HomeController.java
+++ src/main/java/com/heroku/demo/HomeController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Comparator;
@@ -33,7 +34,7 @@ public class HomeController {
     }
 
     @RequestMapping(value = "/glosuj", method = RequestMethod.POST)
-    public String glosuj(ModelMap model, @RequestParam("name") String name, @RequestParam("button") String button) {
+    public String glosuj(ModelMap model, @RequestParam("name") String name, @RequestParam("button") String button, BindingResult result) {
 
         if (!button.contains("refresh")) {
             Option option = null;
